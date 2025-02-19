6b43d7aa874fa7913f0bf1190557663418782d96
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2025-02-19 17:50:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+
+document.getElementById("btn").addEventListener("click" , function() {
+	const inputValue = parseFloat(document.getElementById("ip").value);
+	const outputDiv = document.getElementById("output");
+
+	if(isNaN(inputValue)) {
+		outputDiv.innerHTML  = "Please enter a valid number.";
+		return;
+	}
+
+	outputDiv.innerHTML = "";
+
+	function delayedPromise(value, operation, delay){
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				const result = operation(value);
+				outputDiv.innerHTML += `<p>Result: ${result}</p>`;
+                resolve(result);
+			}, delay)
+		})
+	}
+
+	new Promise((resolve) => {
+        setTimeout(() => {
+            outputDiv.innerHTML += `<p>Result: ${inputValue}</p>`;
+            resolve(inputValue);
+        }, 2000);
+    })
+        .then((value) => delayedPromise(value, (num) => num * 2, 2000))
+        .then((value) => delayedPromise(value, (num) => num - 3, 1000))
+        .then((value) => delayedPromise(value, (num) => num / 2, 1000))
+        .then((value) => delayedPromise(value, (num) => num + 10, 1000))
+        .then((finalValue) => {
+            outputDiv.innerHTML += `<p>Final Result: ${finalValue}</p>`;
+    });
+})
